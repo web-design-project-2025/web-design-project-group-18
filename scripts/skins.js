922f0139eee4cae586c1a58a6f934137a01ded00
FakeLucasNagy <bnlu24vx@student.ju.se>
2025-05-13 18:59:46 +0200

    scroll menu function

---
+++ scripts/skins.js
@@ -0,0 +1,44 @@
+/* Scroll source: https://stackoverflow.com/questions/28576636*/
+
+let mouseDown = false;
+let startX, scrollLeft;
+const slider = document.querySelector('.menu');
+
+const startDragging = (e) => {
+  mouseDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+}
+
+const stopDragging = (e) => {
+  mouseDown = false;
+}
+
+const move = (e) => {
+  e.preventDefault();
+  if(!mouseDown) { return; }
+  const x = e.pageX - slider.offsetLeft;
+  const scroll = x - startX;
+  slider.scrollLeft = scrollLeft - scroll;
+}
+
+// Add the event listeners
+slider.addEventListener('mousemove', move, false);
+slider.addEventListener('mousedown', startDragging, false);
+slider.addEventListener('mouseup', stopDragging, false);
+slider.addEventListener('mouseleave', stopDragging, false);
+
+/* --------------------------------------------------------------------------*/
+
+let isHovered = false;
+
+window.addEventListener('wheel', (event) => {
+    if (!isHovered) return;
+    console.log(isHovered);
+    slider.scrollLeft += event.deltaY * 0.5;
+
+    event.preventDefault();
+}, { passive: false });
+
+slider.addEventListener('mouseenter', () => isHovered = true);
+slider.addEventListener('mouseleave', () => isHovered = false);
